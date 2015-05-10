d73c9e57e9d02250d7f8cb661e52d9e019e25c14
Azianyo <sfrustrowanaherbatamietowa@gmail.com>
2015-05-11 00:07:55 +0200

    Adding records works for all tables

--- add.php
+++ add.php
@@ -7,12 +7,34 @@
 
 	if((isset($_SESSION['login']))&&(md5($_SESSION['login'])==$wiersz['haslo'])&&($_SESSION['nazwisko']==$wiersz['nazwisko'])&&($wiersz['uprawnienia'] == '1')){
 		$tables = array('budynki', 'gabinety', 'lekarze', 'nazwiska', 'specjalnosci');
-		if(isset($_POST['HASLO']) && isset($_POST['NAZWISKO']) && isset($_POST['EMAIL'])) {
-			$add = "INSERT INTO `". $_POST('TABELA') ."`(`nazwisko`, `email`, `haslo`, `uprawnienia`) VALUES (";
-			$add .= "'" . $_POST['NAZWISKO'] . "'" . ",". "'" . $_POST['EMAIL'] . "'" . ","
-			. "'" . md5($_POST['HASLO']) . "'" . ",". "'" . $_POST['UPRAWNIENIA'] . "'". ")";
+		if(isset($_POST['TABELA'])) {
+			$add = "INSERT INTO `". $_POST['TABELA'] ."`(`";
+				foreach($_POST as $key => $val){
+					if(end($_POST) == $val){
+						$add.= $key . "`) VALUES ('";
+					}
+					else if($key == 'TABELA'){}
+					else {
+						$add.= $key . "`, `";
+					}
+				}
+				foreach($_POST as $key => $val){
+					if(end($_POST) == $val){
+						$add.= $val . "')";
+					}
+					else if($key == 'HASLO'){
+						$add .= md5($val) . "', '";;
+					}
+					else if($key == 'TABELA') {}
+					else {
+						$add.= $val . "', '";
+					}
+				}
+				//"nazwisko`, `email`, `haslo`, `uprawnienia`) VALUES (";
+			//$add .= "'" . $_POST['NAZWISKO'] . "'" . ",". "'" . $_POST['EMAIL'] . "'" . ","
+			//. "'" . md5($_POST['HASLO']) . "'" . ",". "'" . $_POST['UPRAWNIENIA'] . "'". ")";
 			$wynik = mysqli_query($mysqli,$add)
-			or die("Błąd");
+			or die($add . mysqli_error($mysqli));
 			if($wynik) {
 				echo "Dodano rekord: " . $add . "<br>";
 			}
@@ -25,6 +47,7 @@
 
 		foreach($tables as $table) {
 			$forma = "<form action=\"add.php\" method=\"POST\">";
+			$forma.="<input type=\"hidden\" name=\"TABELA\" value=\"" . $table."\" size=\"20\" maxlength=\"30\" />";
 			$query = "SELECT * FROM `". $table ."` WHERE 1";
 			echo "<h1>". strtoupper($table) ."</h1>";
 			$sukces = mysqli_query($mysqli,$query)
@@ -34,6 +57,10 @@
 				$row = mysqli_fetch_assoc($sukces);
 				foreach($row as $key => $obj){
 					if($key == 'id'){}
+					else if($key == 'haslo') {
+						$forma.= "Hasło" . ": <input type=\"password\" name=\"". $key ."\" size=\"20\" maxlength=\"30\" /><br>";
+
+					}
 					else {
 						$forma.= $key . ": <input type=\"text\" name=\"". $key ."\" size=\"20\" maxlength=\"30\" /><br>";
 					}
@@ -43,20 +70,15 @@
 			$forma.="</form>";
 			echo $forma;
 		}
+?>	
 
-		?>
-		<form action="add.php" method="POST">
-		Nazwisko: <input type="text" name="NAZWISKO" /><br />
-		E-mail: <input type="text" name="EMAIL" /><br />
-		Hasło: <input type="password" name="HASLO" /><br />
-		Uprawnienia: <input type="text" name="UPRAWNIENIA" /><br />
-		<input type="submit" value="Dodaj rekord" />
-		</form>
 		<form action="log2.php">
 		<input type="submit" value="Wróć" />
 		</form>
-<?
-		}
+
+		<?
+
+	}
 	else {
 		echo "Nie masz uprawnień do tego skryptu";
 	}
